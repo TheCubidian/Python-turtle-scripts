13e42b8944ff1ff6ff094dae82ffc3183a99dc26
The Cubidian <132101268+TheCubidian@users.noreply.github.com>
2026-03-08 15:53:59 +0000

    Finish rainbow circle creation script

---
+++ Turtle Rainbow circle.py
@@ -0,0 +1,13 @@
+from turtle import *
+speed(0)
+colours = ("red", "orange", "yellow", "lightgreen", "dodgerblue", "indigo", "violet")
+radius = (100, 85, 70, 60, 45, 30, 15)
+for a in range (7):
+    penup()
+    goto(0, -radius[a])
+    pendown()
+    fillcolor(colours[a])
+    begin_fill()
+    circle(radius[a])
+    end_fill()
+mainloop()
